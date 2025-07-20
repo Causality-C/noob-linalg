4c765ab6ce3be28fd2ca88f955369b2f8faea91e
Jeffrey Liu <themessenger012@gmail.com>
2025-07-19 21:27:38 -0400

    naive determinant

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
-use std::ops::{Index, IndexMut};
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+    ops::{Index, IndexMut},
+};
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct Matrix {
@@ -26,6 +30,52 @@ impl Matrix {
             rows: size,
             data,
         }
+    }
+    fn from_vec(data: Vec<f64>, rows: usize, cols: usize) -> Self {
+        Self { rows, cols, data }
+    }
+    pub fn from_file(path: &str, rows: usize, cols: usize) -> Self {
+        let file = File::open(path).unwrap();
+        let reader = BufReader::new(file);
+        let mut data: Vec<f64> = Vec::new();
+        for line in reader.lines() {
+            let line = line.unwrap();
+            let iter = line.split_whitespace();
+            for num in iter {
+                data.push(num.parse().unwrap());
+            }
+        }
+        Self { rows, cols, data }
+    }
+    // Do this recursively for now
+    pub fn det(&self) -> f64 {
+        if self.rows != self.cols {
+            panic!("Matrix is not square");
+        }
+        let mut det = 0.0;
+        if self.rows == 1 {
+            det += self.data[0];
+        } else if self.rows == 2 {
+            det += self.data[0] * self.data[3] - self.data[1] * self.data[2];
+        } else {
+            for i in 0..self.rows {
+                let mut vec: Vec<f64> = Vec::new();
+
+                for j in 1..self.rows {
+                    for k in 0..self.cols {
+                        if k == i {
+                            continue;
+                        }
+                        vec.push(self[j][k]);
+                    }
+                }
+
+                let coeff = if i % 2 == 0 { 1.0 } else { -1.0 };
+                det +=
+                    coeff * self[0][i] * Matrix::from_vec(vec, self.rows - 1, self.cols - 1).det();
+            }
+        }
+        det
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use noob_linalg::Matrix;
 fn main() {
-    let mut matrix = Matrix::new(3, 3);
-    let matrix2: Matrix = Matrix::identity(3);
-    matrix[2][2] = 10.0;
-    println!("{:?} {:?}", matrix, matrix2);
+    let matrix = Matrix::from_file("src/3x3.txt", 3, 3);
+    println!("{:?}: {}", matrix, matrix.det());
 }
